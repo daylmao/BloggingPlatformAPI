891abc54618d8e4971238c2e4b959d99e91ff4cd
Dayron <dayronbp06@gmail.com>
2024-12-23 19:28:03 -0400

    add models

---
+++ BloggingPlatformAPI/Models/BlogContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BloggingPlatformAPI.Models
+{
+    public class BlogContext: DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region BlogCategory
+            modelBuilder.Entity<Blog>()
+                .HasOne(b => b.Category)
+                .WithMany(c => c.Blogs)
+                .HasForeignKey(b => b.CategoryId);
+            #endregion
+
+            #region BlogsTags
+            modelBuilder.Entity<Blog>()
+                .HasMany(b => b.Tags)
+                .WithMany(t => t.Blogs)
+                .UsingEntity(n => n.ToTable("BlogsTags"));
+            #endregion
+        }
+    }
+}
