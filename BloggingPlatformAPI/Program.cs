fe52f1fead6b83b53953f7c712db7f00a469e3ef
Dayron <dayronbp06@gmail.com>
2024-12-23 19:20:45 -0400

    add entity configuration

--- BloggingPlatformAPI/Program.cs
+++ BloggingPlatformAPI/Program.cs
@@ -1,3 +1,6 @@
+using BloggingPlatformAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Entity Framework
+builder.Services.AddDbContext<BlogContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BlogConnection")));
+
+// Lazy loading
+builder.Services.AddDbContext<BlogContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlogConnection")).UseLazyLoadingProxies());
 
 var app = builder.Build();
 
