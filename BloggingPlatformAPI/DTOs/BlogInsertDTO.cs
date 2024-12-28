70f56140b4e208b8d378101f4fca644a1510c5ab
Dayron <dayronbp06@gmail.com>
2024-12-28 14:11:55 -0400

    add DTOs

--- BloggingPlatformAPI/DTOs/BlogInsertDTO.cs
+++ BloggingPlatformAPI/DTOs/BlogInsertDTO.cs
@@ -1,6 +1,9 @@
 ﻿namespace BloggingPlatformAPI.DTOs
 {
-    public class BlogDTO
+    public class BlogDTOInsertDTO
     {
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public int CategoryId { get; set; }
     }
 }
--- BloggingPlatformAPI/DTOs/BlogUpdateDTO.cs
+++ BloggingPlatformAPI/DTOs/BlogUpdateDTO.cs
@@ -1,6 +1,10 @@
 ﻿namespace BloggingPlatformAPI.DTOs
 {
-    public class BlogDTO
+    public class BlogDTOUpdateDTO
     {
+        public int BlogId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public int CategoryId { get; set; }
     }
 }
