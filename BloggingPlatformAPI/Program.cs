7e1219c704c1ef5daac9e803c64c21edb72112b6
Dayron <dayronbp06@gmail.com>
2024-12-30 21:23:16 -0400

    update program

--- BloggingPlatformAPI/Program.cs
+++ BloggingPlatformAPI/Program.cs
@@ -1,39 +1,20 @@
-using BloggingPlatformAPI.AutoMapper;
-using BloggingPlatformAPI.DTOs;
-using BloggingPlatformAPI.Models;
-using BloggingPlatformAPI.Repository;
-using BloggingPlatformAPI.Services;
-using BloggingPlatformAPI.Validators;
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
+using BloggingPlatformAPI.Infrastructure.Persistence;
+using BlogginPlatformAPI.Core.Application;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddScoped<ICRUDService<BlogDTO, BlogInsertDTO, BlogUpdateDTO>, BlogService>();
-builder.Services.AddScoped<ICRUDService<CategoryDTO, CategoryInsertDTO, CategoryUpdateDTO>, CategoryService>();
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Validators
-builder.Services.AddValidatorsFromAssemblyContaining<BlogInsertValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<BlogUpdateValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CategoryInsertValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CategoryUpdateValidator>();
 
+//DI
+builder.Services.AddPersistence(configuration);
+builder.Services.AddServices();
 
-// Repository
-builder.Services.AddScoped<IRepository<Blog>, BlogRepository>();
-builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
-
-// Entity Framework
-builder.Services.AddDbContext<BlogContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BlogConnection")));
-
-// automapper
-builder.Services.AddAutoMapper(typeof(MapProfile));
 
 var app = builder.Build();
 
